a237d5fc7ae94c96b5bce67880e917c0a4f1c340
zar <aka.zarj@gmail.com>
2023-08-24 16:11:29 +0600

    relocate

--- src/main/java/com/webApp/webAppAuthorizationDemo/auth/AuthenticationController.java
+++ src/main/java/com/webApp/webAppAuthorizationDemo/auth/AuthenticationController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-#test
+#test3
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
